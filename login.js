270eb09215e4eff2cd3362dbfc14758fbc25d39a
nasar06 <nasar5847@gmail.com>
2022-08-16 10:00:34 +0600

    login system complite

---
+++ login.js
@@ -0,0 +1,12 @@
+//get email input text
+
+// document.getElementById('logBtn').addEventListener('click', function () {
+//     const email = document.getElementById('getInputTxt');
+//     const pass = document.getElementById('getInputPss');
+//     if (email.value === 'nasar5847@gmail.com' && pass.value === 'nasar1928') {
+//         console.log('you are login')
+//     }
+//     else {
+//         console.log('you are not login')
+//     }
+// })
